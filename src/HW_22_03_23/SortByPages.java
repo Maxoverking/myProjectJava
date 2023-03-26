e99244f5440f3c82fc7c789f2a31a313e1a9701f
Maxoverking <avemax7@gmail.com>
2023-03-26 22:21:39 +0200

    add right name

--- src/HW_22_03_23/SortByPages.java
+++ src/HW_22_03_23/SortByPages.java
@@ -6,9 +6,9 @@ public class SortByPages implements Comparator<Book> {
 
     @Override
     public int compare(Book o1, Book o2) {
-        int bookName = o2.getPages() - o1.getPages();
-        if (bookName != 0) {
-            return bookName;
+        int booPages = o2.getPages() - o1.getPages();
+        if (booPages != 0) {
+            return booPages;
         }
         return o1.getBookName().compareToIgnoreCase(o2.getBookName());
     }
