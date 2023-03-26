dad6610bf3d2e2de2259015b54baa18ab70ed57b
Maxoverking <avemax7@gmail.com>
2023-03-26 22:24:25 +0200

    add right bookPages

--- src/HW_22_03_23/SortByPages.java
+++ src/HW_22_03_23/SortByPages.java
@@ -6,9 +6,9 @@ public class SortByPages implements Comparator<Book> {
 
     @Override
     public int compare(Book o1, Book o2) {
-        int booPages = o2.getPages() - o1.getPages();
-        if (booPages != 0) {
-            return booPages;
+        int bookPages = o2.getPages() - o1.getPages();
+        if (bookPages != 0) {
+            return bookPages;
         }
         return o1.getBookName().compareToIgnoreCase(o2.getBookName());
     }
