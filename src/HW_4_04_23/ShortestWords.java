56ffd86ef20e8c7791c49c75908a5619e77054b7
Maxoverking <avemax7@gmail.com>
2023-04-05 17:05:53 +0200

    add HW task2

---
+++ src/HW_4_04_23/ShortestWords.java
@@ -0,0 +1,60 @@
+package HW_4_04_23;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+
+public class ShortestWords {
+//  Задача 2* (не обязательно)
+//  Дана последовательность слов
+//  (количество, а затем и сами слова, каждое слово с новой строки).
+//  Вывести все самые короткие слова через пробел.
+//  Оценить временную и пространственную сложность алгоритма.
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    int n = Integer.parseInt(br.readLine());
+
+    String[] words = new String[n];
+    for (int i = 0; i < n; i++) {
+      words[i] = br.readLine();
+    }
+
+    int shortestLength = Integer.MAX_VALUE;
+    for (String word : words) {
+      if (word.length() < shortestLength) {
+        shortestLength = word.length();
+      }
+    }
+//    System.out.println("shortestLength : "+ shortestLength);
+
+    for (String word : words) {
+      if (word.length() == shortestLength) {
+        System.out.print(word + " ");
+      }
+    }
+  }
+}
+
+
+//public class ShortestWords {
+//  public static void main(String[] args) throws IOException {
+//    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//
+//    int n = Integer.parseInt(br.readLine());
+//
+//    ArrayList<String> words = new ArrayList<>();
+//    for (int i = 0; i < n; i++) {
+//      words.add(br.readLine());
+//    }
+//
+//    int shortestLength = Collections.min(words, Comparator.comparingInt(String::length)).length();
+//    words.removeIf(word -> word.length() > shortestLength);
+//
+//    System.out.println(String.join(" ", words));
+//  }
+//}
+
+
